3543932359379d152d3f23839a97fd24efbf9b34
Martin Jurco <martin.jurco@braiins.cz>
2016-12-10 16:26:23 +0100

    apiary child

---
+++ blog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from blog.models import Post
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+# Create your views here.
